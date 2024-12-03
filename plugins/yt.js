839765e1e44478861ee537635b82857300f7e2af
anilalochna <154051755+anilalochna@users.noreply.github.com>
2024-12-03 23:33:10 +0530

    Create yt.js

---
+++ plugins/yt.js
@@ -0,0 +1,144 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const fg = require('api-dylux')
+const yts = require('yt-search')
+cmd({
+    pattern: "song",
+    desc: "download songs.",
+    category: "download",
+    react: "🎧",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("> 🔗Please give me a URL or TITLE🔗*")
+const search = await yts (q) 
+const data = search.videos[0];
+const url = data.url
+
+let desc = `
+ *❮❮❮ BLACK LEAUGE MD SONG DOWNLOADER ❯❯❯*
+
+> ➤ *𝗧𝗜𝗧𝗟𝗘* - ${data.title}
+
+> ➤ *𝗩𝗜𝗘𝗪𝗦* - ${data.views}
+
+> ➤ *𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗧𝗜𝗢𝗡* - ${data.description}
+
+> ➤ *𝗧𝗜𝗠𝗘* - ${data.timestamp}
+
+> ➤ *𝗔𝗚𝗢* - ${data.ago}
+
+>  downloding & sending plase waite...🚀💗
+
+> *𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘼𝙉𝙄𝙇𝘼 𝙇𝙊𝘾𝙃𝘼𝙉𝘼*
+
+> ↺ |◁   II   ▷|   ♡
+  `
+ await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek})
+ 
+ //download audio
+
+ let down = await fg.yta(url)
+ let downloadUrl = down.dl_url
+    
+ //send audio+ document message 
+
+  conn.ev.on('messages.upsert', async (msgUpdate) => {
+            const msg = msgUpdate.messages[0];
+            if (!msg.message || !msg.message.extendedTextMessage) return;
+
+            const selectedOption = msg.message.extendedTextMessage.text.trim();
+
+            if (msg.message.extendedTextMessage.contextInfo && msg.message.extendedTextMessage.contextInfo.stanzaId === vv.key.id) {
+                switch (selectedOption) {
+                    case '1':
+ await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
+                    break;
+                    case'2':
+ await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"audio/mpeg",fileName:data.title + ".mp3",caption:"> *𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘼𝙉𝙄𝙇𝘼 𝙇𝙊𝘾𝙃𝘼𝙉𝘼*"},{quoted:mek})
+                    break;
+                        default:
+                        reply("Invalid option. Please select a valid option🔴");
+                }
+
+            }
+        });
+  
+} catch (e) {
+console.log(e)
+reply(e)
+}
+});
+
+
+//--------------------------------------VIDEO-DOWNLOAD------------------------------------------------
+//
+cmd({
+    pattern: "video",
+    desc: "download videos.",
+    react: "📽️",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply(">🔗Please give me a URL or TITLE🔗*")
+const search = await yts (q) 
+const data = search.videos[0];
+const url = data.url
+
+let desc = `*❮❮❮ BLACK LEAUGE MD VIDEO DOWNLOADER ❯❯❯*
+
+> ➤ *𝗧𝗜𝗧𝗟𝗘* - ${data.title}
+
+> ➤ *𝗩𝗜𝗘𝗪𝗦* - ${data.views}
+
+> ➤ *𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗧𝗜𝗢𝗡* - ${data.description}
+
+> ➤ *𝗧𝗜𝗠𝗘* - ${data.timestamp}
+
+> ➤ *𝗔𝗚𝗢* - ${data.ago}
+
+> 🎬 Your video downloading....
+
+> *𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘼𝙉𝙄𝙇𝘼 𝙇𝙊𝘾𝙃𝘼𝙉𝘼*
+
+> ↺ |◁   II   ▷|   ♡
+  `
+ await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek})
+ 
+ //download video
+
+ let down = await fg.ytv(url)
+ let downloadUrl = down.dl_url
+    
+ //send video+ document message 
+conn.ev.on('messages.upsert', async (msgUpdate) => {
+            const msg = msgUpdate.messages[0];
+            if (!msg.message || !msg.message.extendedTextMessage) return;
+
+            const selectedOption = msg.message.extendedTextMessage.text.trim();
+
+            if (msg.message.extendedTextMessage.contextInfo && msg.message.extendedTextMessage.contextInfo.stanzaId === vv.key.id) {
+                switch (selectedOption) {
+                    case '1':
+  await conn.sendMessage(from,{video: {url:downloadUrl},mimetype:"video/mp4",caption:"> *𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘼𝙉𝙄𝙇𝘼 𝙇𝙊𝘾𝙃𝘼𝙉𝘼*"},{quoted:mek})
+                    break;
+                    case'2
+  await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"video/mp4",fileName:data.title + ".mp4",caption:"> *𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘼𝙉𝙄𝙇𝘼 𝙇𝙊𝘾𝙃𝘼𝙉𝘼*"},{quoted:mek})
+                   break;
+                  default:
+                        reply("Invalid option. Please select a valid option🔴");
+                }
+
+            }
+        });
+  
+} catch (e) {
+console.log(e)
+reply(e)
+}
+});
+ 
+
